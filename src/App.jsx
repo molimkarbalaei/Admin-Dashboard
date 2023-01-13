f3243ede724ff4a1cbec8388bf6d6af9d88f5933
molim <mohade3.karbalaei@gmail.com>
2023-01-13 19:01:03 +0100

    add theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+
 function App() {
-  return <div className="app"></div>;
+  // use userHook:
+  const [theme, colorMode] = useMode();
+
+  // we accses it to everywhere:
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
